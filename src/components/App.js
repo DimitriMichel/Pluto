6b5cdfc2f981e64b0c32a31447ae4d4d3d3ee000
Dimitri PL <dimitrimachines@gmail.com>
2019-11-18 10:40:20 -0500

    Bar Chart Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import BarChart from "./BarChart";
 import house from "../API/house";
 import senate from "../API/senate";
-import { Layout, Card } from "antd";
+import { Layout, Card, Menu, Row, Col } from "antd";
 import Fuse from "fuse.js";
 import finance from "../API/finance";
+import "./layout.css";
 
-const { Content } = Layout;
+const { Header, Content, Footer } = Layout;
+const { Meta } = Card;
 
 class App extends React.Component {
   state = {
-    representatives: []
+    politician: [],
+    contributions: []
   };
   onTermSubmit = async term => {
     let crp_id;
@@ -60,6 +64,8 @@ class App extends React.Component {
 
     if (representativesSearchResult.length > 0)
       crp_id = representativesSearchResult[0].crp_id;
+    console.log(representativesSearchResult[0]);
+    this.setState({ politician: representativesSearchResult[0] });
 
     // Obtain top ten contributor data, financial summary, personal assets and from returned politician
     const topContributions = await finance.get("/?method=candContrib", {
@@ -67,31 +73,62 @@ class App extends React.Component {
         cid: crp_id
       }
     });
+    console.log(topContributions);
+    const contributorsObject = topContributions.data.response.contributors.contributor;
+    console.log(contributorsObject);
+    const contributorsList = contributorsObject.forEach(contributor => console.log(contributor["@attributes"].org_name)
+    );
 
     const financialSummary = await finance.get("/?method=candSummary", {
       params: {
         cid: crp_id
       }
     });
+    console.log(financialSummary);
     const personalAssets = await finance.get("/?method=memPFDprofile", {
       params: {
         cid: crp_id
       }
     });
+    console.log(personalAssets);
   };
 
   render() {
     return (
       <div>
-        <SearchBar onFormSubmit={this.onTermSubmit} />
-        <Layout style={{ padding: 20 }}>
-          <Content>
-            <Card title="Card title" bordered={false} style={{ width: 300 }}>
-              <p>Card content</p>
-              <p>{this.state.representatives.length}</p>
-              <p>Card content</p>
-            </Card>
+        <Layout className="layout">
+          <Header>
+            <div className="logo" />
+          </Header>
+          <SearchBar onFormSubmit={this.onTermSubmit} />
+          <Content style={{ padding: "0 50px" }}>
+            <div style={{ background: "#fff", padding: 20, minHeight: 280 }}>
+              <Row>
+                <Col span={6}>
+                  <Card
+                    className="logo"
+                    bodyStyle={{ padding: "10px" }}
+                    cover={
+                      <img
+                        alt="example"
+                        src="https://theunitedstates.io/images/congress/original/O000172.jpg"
+                      />
+                    }
+                  >
+                    <Meta className="cardtitle" title="Alexandria" />
+                  </Card>
+                </Col>
+                <BarChart />
+              </Row>
+              <Row>
+                <Col span={6}>col-6</Col>
+                <Col span={6}>col-6</Col>
+                <Col span={6}>col-6</Col>
+                <Col span={6}>col-6</Col>
+              </Row>
+            </div>
           </Content>
+          <Footer style={{ textAlign: "center" }}>Dimitri Michel © 2019</Footer>
         </Layout>
       </div>
     );
---
+++ src/components/BarChart.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {chart} from "frappe-charts/dist/frappe-charts.esm"
+import Col from "antd/es/grid/col";
+
+
+const BarChart = (props) => {
+    return <Col span={18}>BarChart</Col>
+};
+
+export default BarChart
